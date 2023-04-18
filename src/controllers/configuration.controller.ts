20772097d7bb008f38d8308086617022a39364da
Dominik Schmitz <dosc@softwareag.com>
2023-04-18 21:03:26 +0200

    endpoints to change schemas for bs-server and server

--- src/controllers/configuration.controller.ts
+++ src/controllers/configuration.controller.ts
@@ -1,20 +1,82 @@
 import express from 'express';
-import deviceSchema from '../schemas/device.json' assert { type: 'json' };
-import firmwareSchema from '../schemas/firmware.json' assert { type: 'json' };
-import serverSchema from '../schemas/server.json' assert { type: 'json' };
-import requestHandlingSchema from '../schemas/requestHandling.json' assert { type: 'json' };
-import bootstrapServerSchema from '../schemas/boostrapServer.json' assert { type: 'json' };
+import { readJSON, writeJSON } from 'fs-extra/esm';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 export const configurationController = express.Router();
 
-configurationController.get('/schemas', (req: express.Request, res: express.Response) => {
- return res.json(
-    [
+configurationController.get('/schemas', async (req: express.Request, res: express.Response) => {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  try {
+    const deviceSchema = await readJSON(join(__dirname, '../schemas/device.json'));
+    const serverSchema = await readJSON(join(__dirname, '../schemas/server.json'));
+    const bootstrapServerSchema = await readJSON(join(__dirname, '../schemas/bootstrapServer.json'));
+    const firmwareSchema = await readJSON(join(__dirname, '../schemas/firmware.json'));
+    const requestHandlingSchema = await readJSON(join(__dirname, '../schemas/requestHandling.json'));
+    return res.json([
       deviceSchema,
       serverSchema,
       bootstrapServerSchema,
       firmwareSchema,
       requestHandlingSchema
-    ]
-  )
+    ]);
+  } catch(err) {
+    const e = err as Error;
+    return res.status(400).send(e.message);
+  }
+});
+
+function updateJson(filename: string, body: unknown) {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  return writeJSON(join(__dirname, `../schemas/${filename}.json`), body);
+}
+
+configurationController.put('/schemas/bs-server', async (req: express.Request, res: express.Response) => {
+  try {
+    await updateJson('bootstrapServer', req.body);
+    return res.status(200).send(req.body);
+  } catch(err) {
+    const e = err as Error;
+    return res.status(400).send(e.message);
+  }
+});
+
+configurationController.put('/schemas/server', async (req: express.Request, res: express.Response) => {
+  try {
+    await updateJson('server', req.body);
+    return res.status(200).send(req.body);
+  } catch(err) {
+    const e = err as Error;
+    return res.status(400).send(e.message);
+  }
+});
+
+async function resetSchema(filename: string) {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  try {
+    const source = await readJSON(join(__dirname, `../schemas/default/${filename}.json`));
+    await writeJSON(join(__dirname, `../schemas/${filename}.json`), source);
+    return source;
+  } catch (err) {
+    throw err as Error;
+  } 
+}
+
+configurationController.get('/schemas/reset/server', async (req: express.Request, res: express.Response) => {
+  try {
+    return res.status(200).send(await resetSchema('server'));
+  } catch(err) {
+    const e = err as Error;
+    return res.status(400).send(e.message);
+  }
+});
+
+configurationController.get('/schemas/reset/bs-server', async (req: express.Request, res: express.Response) => {
+  try {
+    return res.status(200).send(await resetSchema('bootstrapServer'));
+  } catch(err) {
+    const e = err as Error;
+    return res.status(400).send(e.message);
+  }
 });
