5a5b45b0517fd217666c149c76a8ea6d2e59752f
Dominik Schmitz <dosc@softwareag.com>
2023-04-24 17:16:17 +0200

    add connectivity endpoints

--- src/controllers/configuration.controller.ts
+++ src/controllers/configuration.controller.ts
@@ -10,73 +10,34 @@ configurationController.get('/schemas', async (req: express.Request, res: expres
 
   try {
     const deviceSchema = await readJSON(join(__dirname, '../schemas/device.json'));
-    const serverSchema = await readJSON(join(__dirname, '../schemas/server.json'));
-    const bootstrapServerSchema = await readJSON(join(__dirname, '../schemas/bootstrapServer.json'));
     const firmwareSchema = await readJSON(join(__dirname, '../schemas/firmware.json'));
     const requestHandlingSchema = await readJSON(join(__dirname, '../schemas/requestHandling.json'));
     return res.json([
       deviceSchema,
-      serverSchema,
-      bootstrapServerSchema,
       firmwareSchema,
       requestHandlingSchema
     ]);
   } catch(err) {
-    const e = err as Error;
-    return res.status(400).send(e.message);
+    return res.status(400).send((err as Error).message);
   }
 });
 
-function updateJson(filename: string, body: unknown) {
-  const __dirname = dirname(fileURLToPath(import.meta.url));
-  return writeJSON(join(__dirname, `../schemas/${filename}.json`), body);
-}
-
-configurationController.put('/schemas/bs-server', async (req: express.Request, res: express.Response) => {
-  try {
-    await updateJson('bootstrapServer', req.body);
-    return res.status(200).send(req.body);
-  } catch(err) {
-    const e = err as Error;
-    return res.status(400).send(e.message);
-  }
-});
-
-configurationController.put('/schemas/server', async (req: express.Request, res: express.Response) => {
-  try {
-    await updateJson('server', req.body);
-    return res.status(200).send(req.body);
-  } catch(err) {
-    const e = err as Error;
-    return res.status(400).send(e.message);
-  }
-});
-
-async function resetSchema(filename: string) {
+configurationController.get('/schemas/connectivity', async (req: express.Request, res: express.Response) => {
   const __dirname = dirname(fileURLToPath(import.meta.url));
   try {
-    const source = await readJSON(join(__dirname, `../schemas/default/${filename}.json`));
-    await writeJSON(join(__dirname, `../schemas/${filename}.json`), source);
-    return source;
-  } catch (err) {
-    throw err as Error;
-  } 
-}
-
-configurationController.get('/schemas/reset/server', async (req: express.Request, res: express.Response) => {
-  try {
-    return res.status(200).send(await resetSchema('server'));
+    const connectivitySchema = await readJSON(join(__dirname, '../schemas/connectivity.json'));
+    return res.json(connectivitySchema);
   } catch(err) {
-    const e = err as Error;
-    return res.status(400).send(e.message);
+    return res.status(400).send((err as Error).message);
   }
 });
 
-configurationController.get('/schemas/reset/bs-server', async (req: express.Request, res: express.Response) => {
+configurationController.put('/schemas/connectivity', async (req: express.Request, res: express.Response) => {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
   try {
-    return res.status(200).send(await resetSchema('bootstrapServer'));
+    await writeJSON(join(__dirname, `../schemas/connectivity.json`), req.body);
+    return res.status(200).send(req.body);
   } catch(err) {
-    const e = err as Error;
-    return res.status(400).send(e.message);
+    return res.status(400).send((err as Error).message);
   }
 });
